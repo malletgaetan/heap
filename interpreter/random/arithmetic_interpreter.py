a49f7d0869d6681c34a2869f93d986f2c0d5a14d
malletgaetan <mallet.gaetan@hotmail.fr>
2022-05-30 04:25:09 +0200

    cleaner 6

--- interpreter/random/arithmetic_interpreter.py
+++ interpreter/random/arithmetic_interpreter.py
@@ -1,71 +1,77 @@
-# pos should be equal to the current pos of pointer
-def get_next_character(text, pos):
-   if pos is None:
-      pos = 0
-   else:
-      pos += 1
-   while(pos < len(text) and text[pos].isspace()):
-      pos += 1
-   if pos >= len(text):
-      return None, pos
-   return text[pos], pos
+# exercise part 6
+class Interpreter():
+   def __init__(self, text):
+      self.text = text
+      self.pos = 0
+      self.current_char = text[0]
 
-def test_integer(character, pos):
-   if character is None or not character.isdigit():
-      raise Exception(f'expected a single digit integer at pos {pos + 1} got {character}')
+   def to_next_token(self):
+      text = self.text
+      # move
+      self.pos += 1
 
-def future(calculus, pos):
-   current_char, pos = get_next_character(calculus, pos)
-   if current_char == '(':
-      return calc(calculus, pos)
-   test_integer(current_char, pos)
-   return int(current_char), current_char, pos
+      # skip whitespaces
+      while(self.pos < len(text) and text[self.pos].isspace()):
+         self.pos += 1
 
-def mult_div(calculus, pos):
-   # can get either ( or INTEGER
-   acc, current_char, pos = future(calculus, pos)
-   # can get either DIVIDE or MULTIPLY
-   current_char, pos = get_next_character(calculus, pos)
-   while(current_char is not None and current_char in '*/'):
-      operator = current_char
+      if self.pos >= len(text):
+         self.current_char = None
+         return
 
-      if operator == '*':
-         res, current_char, pos = future(calculus, pos)
-         acc *= res
-      if operator == '/':
-         res, current_char, pos = future(calculus, pos)
-         acc /= res
+      self.current_char = text[self.pos]
 
-      current_char, pos = get_next_character(calculus, pos)
+   # factor=INTEGER|S_PARENTHESIS expr S_PARENTHESIS
+   def factor(self):
+      if self.current_char == '(':
+         self.to_next_token()
+         res = self.expr()
+         self.to_next_token()
+         return res
+      if self.current_char.isdigit():
+         current = self.current_char
+         self.to_next_token()
+         return int(current)
 
-   return acc, current_char, pos
+      raise Exception(f'expected either INTEGER or ( at position {self.pos} got {self.current_char}')
 
-# take a string of addition and soustration, interpet it and give result
-def calc(calculus, pos = None):
-   # can get either ( or INTEGER
-   acc, current_char, pos = mult_div(calculus, pos)
-   # can get either PLUS or MINUS
-   while(current_char is not None and current_char != ')' and current_char in '+-'):
-      # current_char should be an operator
-      operator = current_char
+   # term=factor((PLUS|MINUS)factor)*
+   def term(self):
+      acc = self.factor()
+
+      while(self.current_char is not None and self.current_char in '*/'):
+         operator = self.current_char
+
+         self.to_next_token()
+
+         if operator == '*':
+            acc *= self.factor()
+         if operator == '/':
+            acc /= self.factor()
+         
+      return acc
+
+   # expr=term((PLUS|MINUS)term)*
+   def expr(self):
+      acc = self.term()
+
+      while(self.current_char is not None and self.current_char in '+-'):
+         operator = self.current_char
+
+         self.to_next_token()
+
+         if operator == '+':
+            acc += self.term()
+         if operator == '-':
+            acc -= self.term()
       
-      # pos here is pointing either to an INTEGER or an OPENED PARENTHESIS
-      if operator == '-':
-         res, current_char, pos = mult_div(calculus, pos)
-         acc -= res
-      if operator == '+':
-         res, current_char, pos = mult_div(calculus, pos)
-         acc += res
-
-      print(f'after={current_char}')
-
-   return acc, current_char, pos
+      return acc
 
 while True:
    calculus = input('calc> ')
    try:
-      res, _, _ = calc(calculus)
-      print(res)
+      interpreter = Interpreter(calculus)
+      result = interpreter.expr()
+      print(result)
    except EOFError:
       break
    if not calculus:
